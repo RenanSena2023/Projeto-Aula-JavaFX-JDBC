fd96204307cb908915fba194d1f665f11160ebdb
RenanSena2023 <renansena20152proton.me>
2023-02-11 16:35:16 -0300

    Department

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,6 +25,7 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);
 			
 			mainScene = new Scene(scrollPane);
+			
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
